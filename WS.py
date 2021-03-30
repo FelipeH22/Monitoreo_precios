f95ac49d00667dc6e93e44cb958007619daabf44
FelipeH22 <juan22herrera01@gmail.com>
2021-03-29 23:20:59 -0500

    Se modifica WS

--- WS.py
+++ WS.py
@@ -4,10 +4,10 @@ from bs4 import BeautifulSoup
 URL = 'https://www.alkosto.com/llanta-pirelli-p1-185-65r15'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
-job_elems = soup.find_all('span', class_='price')
-for job_elem in job_elems:
-  job_elem=str(job_elem).split("span")
-  for x in job_elem:
+sentencias = soup.find_all('span', class_='price')
+for sentencia in sentencias:
+  sentencia=str(sentencia).split("span")
+  for x in sentencia:
     if "itemprop=\"price\">" in x:
       precio=x.split(">")[1].rsplit("<")[0]
 print("Precio del producto: "+precio)
