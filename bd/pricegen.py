4a79b79d3f3edc1392a57203dfe6e4c507a39818
FelipeH22 <juan22herrera01@gmail.com>
2021-06-30 17:52:19 -0500

    Cambios en bd

--- bd/pricegen.py
+++ bd/pricegen.py
@@ -1,5 +1,4 @@
 import conecta
-import random
 from datetime import datetime
 from ws import WS
 import producto
@@ -12,18 +11,23 @@ def insert_precio(precio,id_producto):
     curs.execute("INSERT INTO precios (precio,fecha,id_producto) VALUES (?,?,?)", valores)
     con.commit()
 
-                        
-with open("../links_pruebas.txt") as archivo:
-    archivo.read()
-    archivo=archivo.split("\n")
-    for link in archivo:
-        print(link)
-        contador=7
-        try:
-            precio=int("".join(WS.get_producto(link).split(".")))
-            id_pr=int(contador)
-            print(precio,contador)
-            insert_precio(precio,contador)
-            print("Sí se pudo")
-            contador+=1
-        except Exception as e: print("No se pudo",e)
+def get_producto(url):
+    curs=con.cursor()
+    curs.execute("SELECT id FROM productos WHERE url= ?",(url,))
+    return curs.fetchone()
+
+
+archivo=open("../links_pruebas.txt").read()
+archivo=archivo.split("\n")
+for link in archivo:
+    print(link)
+    contador=int(get_producto(link)[0])
+    print(contador)
+    try:
+        precio=int("".join(WS.get_producto(link).split(".")))
+        id_pr=int(contador)
+        print(precio,contador)
+        insert_precio(precio,contador)
+        print("Sí se pudo")
+        contador+=1
+    except Exception as e: print("No se pudo",e)
