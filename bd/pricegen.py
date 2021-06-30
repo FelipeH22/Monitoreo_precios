c65eb20ea4b602c67f718b2645eb962276a91634
FelipeH22 <juan22herrera01@gmail.com>
2021-06-30 17:40:42 -0500

    Se modifica comparación entre listas doblemente enlazadas y arreglos

--- bd/pricegen.py
+++ bd/pricegen.py
@@ -11,35 +11,19 @@ def insert_precio(precio,id_producto):
     valores=(precio,fecha,id_producto)
     curs.execute("INSERT INTO precios (precio,fecha,id_producto) VALUES (?,?,?)", valores)
     con.commit()
-"""
-for _ in range(1,201):
-    id_pr=random.randint(1,30)
-    promocion=random.randint(0,99999)
-    precio=producto.get_precio_actual(id_pr)
-    precio+=50000
-    if precio-promocion>precio/2:
-        precio=precio-promocion
-    else:
-        precio=precio+50000
-    try:
-        insert_precio(precio,id_pr)
-        print("se ha añadido el registo",id_pr, precio)
-    except Exception as e: print("No se puede insertar",e)
 
-
-
-"""
                         
-archivo=open("../links_pruebas.txt").read()
-archivo=archivo.split("\n")
-for link in archivo:
-    print(link)
-    contador=7
-    precio=int("".join(WS.get_producto(link).split(".")))
-    id_pr=int(contador)
-    print(precio,contador)
-    try:
-        insert_precio(precio,contador)
-        print("Sí se pudo")
-        contador+=1
-    except Exception as e: print("No se pudo",e)
+with open("../links_pruebas.txt") as archivo:
+    archivo.read()
+    archivo=archivo.split("\n")
+    for link in archivo:
+        print(link)
+        contador=7
+        try:
+            precio=int("".join(WS.get_producto(link).split(".")))
+            id_pr=int(contador)
+            print(precio,contador)
+            insert_precio(precio,contador)
+            print("Sí se pudo")
+            contador+=1
+        except Exception as e: print("No se pudo",e)
