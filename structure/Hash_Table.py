344d407c0e0417a86d51bc1b2c9d3e15f5c18951
CarlosJulian3112 <corderoguevaracarlosjulian@gmail.com>
2021-09-04 17:59:57 -0500

    Create Hash_Table.py

---
+++ structure/Hash_Table.py
@@ -0,0 +1,41 @@
+import random
+class hash_table:
+  def __init__(self, m):
+    self.p = 10000019
+    self.a = random.randint(1,self.p-1)
+    self.b = random.randint(0,self.p-1)
+    self.m = m
+    self.table = [None] * self.m
+  def Hash_func(self, x):
+    if (type(x) == int):
+      hash = ((self.a*x+self.b)%self.p)%self.m
+    else:
+      hash = 0
+      for i,j in enumerate(x):
+        hash += (ord(j)*(self.a^i))%self.p
+      hash = hash%self.m
+    return hash
+  def Insert(self, x):
+    hash = self.Hash_func(x.key)
+    if self.table[hash] == None:
+      list2 = []
+      list2.append(x)
+      self.table[hash] = list2
+    else:
+      self.table[hash].append(x)
+  def Search(self,x):
+    hash = self.Hash_func(x)
+    if (self.table[hash] != None):
+      for i in self.table[hash]:
+        if i.key == x:
+          return i.value
+    return None
+  def Remove(self, x):
+    hash = self.Hash_func(x)
+    if (self.table[hash] != None):
+      for i in self.table[hash]:
+        if i.key == x:
+          self.table[hash].remove(i)
+          print("Elemento Eliminado Correctamente")
+          return
+    print("No se encontro este elemento")
---
+++ structure/Node_Hash.py
@@ -0,0 +1,4 @@
+class node_hash:
+  def __init__(self,k,v):
+    self.key = k
+    self.value = v
