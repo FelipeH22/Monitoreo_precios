946c284d92d9a66cda912be5355943677fdb4f00
FelipeH22 <juan22herrera01@gmail.com>
2021-09-05 22:52:33 -0500

    Se añade prueba de tiempo en heap

---
+++ structure/precios_estructuras.py
@@ -0,0 +1,20 @@
+from Heap import Heap
+import sys
+sys.path.append('../bd')
+from bd import precio
+from ws import ws
+import time
+
+url=input()
+precios=precio.get_precios_producto(url)
+heap_producto1=Heap()
+for pr in precios: heap_producto1.Insert(pr)
+t_0=time.time()
+print(f"El precio mínimo registrado para {ws.get_nombre(url)} es: {str(heap_producto1.ExtractMin())[1:-3]}")
+print('tiempo usando heap:',time.time()-t_0)
+t_0_1=time.time()
+menor=99999999
+for pr in precios:
+    if int(str(pr)[1:-3])<menor: menor=int(str(pr)[1:-3])
+print(f"El precio mínimo registrado para {ws.get_nombre(url)} es: {menor}")
+print('tiempo usando for:',time.time()-t_0_1)
